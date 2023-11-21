104b37bdb4f087286fae551db9291e908d4365a7
lademir <lademirjunior@hotmail.com>
2023-11-20 22:20:57 -0300

    Loading animation

--- web/src/app/login/page.tsx
+++ web/src/app/login/page.tsx
@@ -7,13 +7,31 @@ import { Metadata, NextPage } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 const LoginPage: NextPage = () => {
 
     const router = useRouter();
 
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
     const { register, handleSubmit, formState: { errors } } = useForm();
+
+    async function onSubmit(event: React.BaseSyntheticEvent) {
+        event.preventDefault();
+        setIsLoading(true);
+
+        handleSubmit((data) => {
+            console.log(data);
+        });
+
+        setTimeout(() => {
+            setIsLoading(false);
+            router.push("/");
+        }, 3000);
+    }
 
     return (
         <main className="flex flex-col items-center justify-center h-screen">
@@ -22,8 +40,8 @@ const LoginPage: NextPage = () => {
                     <Image src="/logo.png" alt="BoraTreinar" className="m-auto" width={200} height={200} />
                 </CardHeader>
                 <CardContent className="text-slate-900">
-                    <form className="" onSubmit={handleSubmit((data) => alert(data))}>
-                        <Input type="email" placeholder="E-mail" className="w-full" {...register('email',
+                    <form className="" onSubmit={onSubmit}>
+                        <Input type="email" placeholder="E-mail" className="w-full focus-visible:ring-offset-0" {...register('email',
                             {
                                 required: 'E-mail obrigatório',
                             }
@@ -31,18 +49,24 @@ const LoginPage: NextPage = () => {
                         {
                             <span className="text-red-500 text-xs">{errors?.email?.message?.toString()}</span>
                         }
-                        <Input type="password" placeholder="Senha" className="w-full mt-2" {...register('password', {
+                        <Input type="password" placeholder="Senha" className="w-full mt-2 focus-visible:ring-offset-0" {...register('password', {
                             required: "Senha obrigatória"
                         })} />
                         {
                             errors.password && <span className="text-red-500 text-xs">{errors.password.message?.toString()}</span>
                         }
-                        <div className="flex flex-col mt-5">
-                            <Link className="text-slate-300 text-xs mb-2 hover:text-slate-50 transition duration-200" href="/forgot-password">
-                                Esqueceu a senha?
-                            </Link>
-                            <Button type="submit" className="w-full">Entrar</Button>
-                            <Button type="button" onClick={() => router.push('/register')} className="w-full mt-3" variant="secondary">Cadastrar</Button>
+                        <div className="flex flex-col mt-1">
+                            <span className="mb-4">
+                                <Link className="text-slate-300 text-xs  min-w-fit hover:text-slate-50 transition duration-200" href="/forgot-password">
+                                    Esqueceu a senha?
+                                </Link>
+                            </span>
+                            <Button disabled={isLoading} type="submit" className="w-full">
+                                {
+                                    isLoading ? <AiOutlineLoading3Quarters className="animate-spin" /> : "Entrar"
+                                }
+                            </Button>
+                            <Button disabled={isLoading} type="button" onClick={() => router.push('/register')} className="w-full mt-3" variant="secondary">Cadastrar</Button>
                         </div>
                     </form>
                 </CardContent>
